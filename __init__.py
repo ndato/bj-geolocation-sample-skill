0ababd260e1495e81b35149497f19b24a574a263
ndato <khryss812c@gmail.com>
2019-11-05 13:49:33 +0800

    Initial Commit

--- __init__.py
+++ __init__.py
@@ -1,13 +1,22 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from mycroft.api import GeolocationApi
+import pprint
 
 class BjGeolocationSample(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        
+    def initialize(self):
+        self.geolocation_api = GeolocationApi()
 
     @intent_file_handler('sample.geolocation.bj.intent')
     def handle_sample_geolocation_bj(self, message):
-        self.speak_dialog('sample.geolocation.bj')
+        loc_string = message.data.get('location')
+        self.log.info(loc_string)
+        location = self.geolocation_api.get_geolocation(loc_string)
+        
+        pprint.pprint(location)
+        self.speak_dialog('sample.geolocation.bj', {'location': loc_string})
 
 
 def create_skill():
